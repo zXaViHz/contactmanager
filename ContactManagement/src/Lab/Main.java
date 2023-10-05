48f0d0db008571ff92d9bc9571908facd94e96e5
zXaViHz <144187401+zXaViHz@users.noreply.github.com>
2023-10-06 00:17:32 +0700

    Add files via upload

---
+++ ContactManagement/src/Lab/Main.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author dell
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<Contact> lc = new ArrayList<>();
+        lc.add(new Contact(1, "Iker Casillas", "Star", "Spain", "1234567890", "Iker", "Casillas"));
+        lc.add(new Contact(2, "John Terry", "Star", "England", "1234567890", "John", "Terry"));
+        lc.add(new Contact(3, "Raul Gonzalez", "Star", "Spain", "1234567890", "Raul", "Gonzalez"));
+        //loop until user want to exit
+        while (true) {
+            int choice = Manager.menu();
+            switch (choice) {
+                case 1:
+                    Manager.createContact(lc);
+                    break;
+                case 2:
+                    Manager.printAllContact(lc);
+                    break;
+                case 3:
+                    Manager.deleteContactd(lc);
+                    break;
+                case 4:
+                    return;
+            }
+        }
+    }
+}
